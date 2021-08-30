ef5a2b886104c7ffbfcdcb3b7f388117aff4ea89
Somdotta07 <jhilick.s@gmail.com>
2021-08-30 03:42:33 +0200

    another with Jquery
    added a few jquery code
    changed design

--- ChatBot Project/script.js
+++ ChatBot Project/script.js
@@ -1,107 +1,105 @@
-const inputTextArea = get (".chat-input");
 
-const inputText = get (".input-msg");
-const chatBody = get (".main-body");
-
-const Boo_Msgs =
- ["Welcome to my chat room.",
-"How are you?",'Nice to meet you',"What do you do?",
-"Sorry, I don't understand your message.",
-"Thanks for spending time with me.",
-"Bye, See you soon."];
-
-const Boo_Name = "Boo";
-const Person_Name ="Anonymous";
+$(document).ready(() => {
 
 
 
 
-//Typing and Sending msg
-
-//inputTextArea.addEventListener ("submit",event => {     
-   // event.preventDefault();
-
-   // const newText =inputText.value;
-   // if(!newText) return;
-
-    appendMessage(Person_Name,"right",newText);
-   
-    inputText.value = "";
-    booReply();
-   // appendMessage(Person_Name,"right",newText);
-   
-   // inputText.value = "";
-   // booReply();
-//});
-
-//
-    $(function(){
-    $("sending-msg").click(function(e){
-        e.preventDefault();
-
-        const newText =$(".input-msg").value();
-         if(!newText) return;
-
-        appendMessage(Person_Name,"right",newText);
-   
-          inputText.value = "";
-         booReply();
+    let Boo_rply = [`Welcome to my chat room.`,
+  `How are you?`,
+  `Nice to meet you`,
+  `What do you do?`,
+  `Thanks for your response`,
+  `Sorry, I don't understand your message.`,
+  `Thanks for spending time with me.`,
+  `Bye, See you soon.`,
+  `Sorry, I don't understand you.`,
+  `Please try again`],
+  i=0;
+  
+  //User input
+  
+  
+      function $postMessage() {
+  
+          $("#inputmsg").find("br").remove();
+  
+          let $inputmsg = $("#inputmsg").html().trim(); // get text from text box
+            $inputmsg = $inputmsg.replace(/<div>/, "<br>").replace(/<div>/g, "").replace(/<\/div>/g, "<br>").replace(/<br>/g, " ").trim();
+          if ($inputmsg) { // if textbox is not empty
+              const newText = `<div class="post user-post" style="float:right"> <p class="chat-name">Anonymous</p>${$inputmsg}</div>`; // convert post to html
+              $("#main-body").append(newText); // add post to main-body
+        formatDate();
+              $scrollDown(); // stay at bottom of chat
+  
+          };
+          $("#inputmsg").empty();
+      setTimeout(function() {
+          booReply();
+        }, 1000 + Math.random() * 20 * 100);
+  };
+  
+  // Chat Enter/Send---from Codepen
+      $("#inputmsg").on("keyup", (event) => {
+          if (event.which === 13) $postMessage();
+      }).on("focus", () => {
+          $("#inputmsg").addClass("focus");
+      }).on("blur", () => {
+          $("#inputmsg").removeClass("focus");
+      });
+      $("#send").on("click", $postMessage);
+  
+  
+  
+  
+  // ------Random reply-----
+  
+  function booReply(){
+    if($("#inputmsg").val() != ""){
+      return false;
+    }
+    setTimeout(function(){
+    $(`<div class=" post boo-post" ><p class="chat-name">Boo</p>` + Boo_rply[i]+ `</div>`).appendTo($("#main-body")).addClass("new");
+      formatDate();
+          $scrollDown();
+        i++;
+      });
+  };
+    //-----Date & Time------
+  
+    function formatDate(today) {
+  
+     var today = new Date();
+  
+     var h= today.getHours();
+      var m=today.getMinutes();
+      if(m<10){
+        m="0"+m;
+      }
+      var t = h+":"+ m;
+  
+     $('<div class="dateTime" style="font-size: 0.6em">' + t +'</div>').appendTo($(".post:last"));
+  
+  
+    }
+  //----Scroll------
+  
+      function $scrollDown() {
+  
+  
+    $("#main-body").stop().animate({
+      scrollTop: $("#main-body")[0].scrollHeight
     });
-});
-
-//Showing msg in the body
-function appendMessage(name,side,text){
-    const textHTML =` <div class="chat ${side}-msg">
-                        <div class="chat-bubble">
-                            <div class="chat-info">
-                             <div class="chat-info-name">${name}</div>
-                                
-                            </div>
-                            <div class="chat-text">${text}</div>
-                            <div class="datetime">${formatDate(new Date())}</div>
-                        </div>  
-                    </div>`;
-
-    
-    
-
-
-    chatBody.insertAdjacentHTML("beforeend",textHTML);
- 
- 
-    chatBody.scrollTop = chatBody.scrollHeight; 
-}
-
-// Automatic reply
- 
-function booReply(){
-    const rply= random(0, Boo_Msgs.length-1);
-
-    const newText =Boo_Msgs[rply];
-    const delay  = newText.split(" ").length * 10;
-
-    setTimeout(()=>{
-        appendMessage(Boo_Name,"left",newText);
-    }, delay);
-}
-//- DateTime -Took help from phoenixnap.com---
-function formatDate(today) {
+      }
   
-   var today = new Date();
-
-   var dt = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-   
-   var t= today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-   
-   var dateTime = dt+ ' '+ t;
-   return `${dateTime}`;
-
-  }
+    //----setTimeout---
   
-function get(selector, root = document) { // Copied from Codepen
-    return root.querySelector(selector);
-  }
+      setTimeout(function(){
+        booReply();
+      },50);
+  
+  
 
-  function random(a, b) {
-    return Math.floor(Math.random() * (b -a) + a);
-  }
+  
+  
+  });
+  
